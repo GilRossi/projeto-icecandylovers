9f0c403ee0d7069a7fb70a8f34a040db01810e54
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-03-14 20:33:46 -0300

    config terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1"
 }
-provider "tls" {}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,3 @@
-variable "ssh_private_key" {
-  type        = string
-  description = "Chave privada SSH para autenticação."
-  sensitive   = true
-}
-variable "ssh_public_key" {
-  type        = string
-  description = "Chave pública SSH para acessar a instância."
-  validation {
-    condition     = length(var.ssh_public_key) > 0
-    error_message = "A chave pública SSH não pode estar vazia."
-  }
-}
-
-variable "aws_region" {
-  description = "A região da AWS"
-  type        = string
-  default     = "us-east-1"
+variable "bucket_name" {
+  type = string
 }
